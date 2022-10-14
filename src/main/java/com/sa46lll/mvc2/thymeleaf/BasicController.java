2507514a1c67812155a04ef1faca2ba431f5351b
sa46lll <9812smh@naver.com>
2022-10-14 12:19:53 +0000

    feat: SpringEL

--- src/main/java/com/sa46lll/mvc2/thymeleaf/BasicController.java
+++ src/main/java/com/sa46lll/mvc2/thymeleaf/BasicController.java
@@ -1,5 +1,9 @@
 package com.sa46lll.mvc2.thymeleaf;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import lombok.Getter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,5 +23,34 @@ public class BasicController {
     public String textUnescaped(Model model) {
         model.addAttribute("data", "Hello <b>sa46lll!</b>");
         return "basic/text-unescaped";
+    }
+
+    @GetMapping("/variable")
+    public String variable(Model model) {
+        User userA = new User("sa46lll", 10);
+        User userB = new User("sa46lll2", 20);
+
+        List<User> users = List.of(userA, userB);
+
+        Map<String, User> map = new HashMap<>();
+        map.put("userA", userA);
+        map.put("userB", userB);
+
+        model.addAttribute("user", userA);
+        model.addAttribute("users", users);
+        model.addAttribute("userMap", map);
+
+        return "basic/variable";
+    }
+
+    @Getter
+    static class User {
+        private String username;
+        private int age;
+
+        public User(String username, int age) {
+            this.username = username;
+            this.age = age;
+        }
     }
 }
