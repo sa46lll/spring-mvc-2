329273b5eb071acf4bc2f1e2161653ce76b66e32
sa46lll <9812smh@naver.com>
2022-10-18 12:19:53 +0000

    feat: 기본객체, 편의객체

--- src/main/java/com/sa46lll/mvc2/thymeleaf/BasicController.java
+++ src/main/java/com/sa46lll/mvc2/thymeleaf/BasicController.java
@@ -3,7 +3,9 @@ package com.sa46lll.mvc2.thymeleaf;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import javax.servlet.http.HttpSession;
 import lombok.Getter;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -43,6 +45,12 @@ public class BasicController {
         return "basic/variable";
     }
 
+    @GetMapping("/basic-objects")
+    public String basicObjects(HttpSession session) {
+        session.setAttribute("sessionData", "Hello Session");
+        return "basic/basic-objects";
+    }
+
     @Getter
     static class User {
         private String username;
@@ -51,6 +59,13 @@ public class BasicController {
         public User(String username, int age) {
             this.username = username;
             this.age = age;
+        }
+    }
+
+    @Component("helloBean")
+    static class HelloBean {
+        public String hello(String data) {
+            return "Hello " + data;
         }
     }
 }
