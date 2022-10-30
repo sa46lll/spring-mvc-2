1d9569cf6aed3e1e6bd769ace53b00ae689faffd
sa46lll <9812smh@naver.com>
2022-10-30 12:19:53 +0000

    feat: javascript inline

--- src/main/java/com/sa46lll/mvc2/thymeleaf/BasicController.java
+++ src/main/java/com/sa46lll/mvc2/thymeleaf/BasicController.java
@@ -108,6 +108,13 @@ public class BasicController {
         return "basic/block";
     }
 
+    @GetMapping("/javascript")
+    public String javascript(Model model) {
+        model.addAttribute("user", new User("sa46lll", 10));
+        addUsers(model);
+        return "basic/javascript";
+    }
+
     @Getter
     private static class User {
         private String username;
