a33669c5706cc52cf2c7d878866d25861c76d5f2
sa46lll <9812smh@naver.com>
2023-02-13 03:23:11 +0900

    feat: selectbox

--- src/main/java/com/sa46lll/mvc2/itemservice/web/form/FormItemController.java
+++ src/main/java/com/sa46lll/mvc2/itemservice/web/form/FormItemController.java
@@ -1,8 +1,10 @@
 package com.sa46lll.mvc2.itemservice.web.form;
 
+import com.sa46lll.mvc2.itemservice.domain.item.DeliveryCode;
 import com.sa46lll.mvc2.itemservice.domain.item.Item;
 import com.sa46lll.mvc2.itemservice.domain.item.ItemRepository;
 import com.sa46lll.mvc2.itemservice.domain.item.ItemType;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -44,6 +46,15 @@ public class FormItemController {
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
         return "form/items";
+    }
+
+    @ModelAttribute("deliveryCodes")
+    public List<DeliveryCode> deliveryCodes() {
+        List<DeliveryCode> deliveryCodes = new ArrayList<>();
+        deliveryCodes.add(new DeliveryCode("FAST", "빠른 배송"));
+        deliveryCodes.add(new DeliveryCode("NORMAL", "일반 배송"));
+        deliveryCodes.add(new DeliveryCode("SLOW", "느린 배송"));
+        return deliveryCodes;
     }
 
     @GetMapping("/{itemId}")
